fee7c065a3dcde932ebfe19b8f8971b593ff02ae
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-08-07 19:08:57 +0200

    Add a PSR18 caching HTTP client.

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -12,6 +12,8 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use ChampsLibres\WopiBundle\EventListener\WopiDiscoveryListener;
 use ChampsLibres\WopiLib\Discovery\WopiDiscovery;
 use ChampsLibres\WopiLib\Discovery\WopiDiscoveryInterface;
+use Symfony\Component\HttpClient\CachingHttpClient;
+use Symfony\Component\HttpClient\Psr18Client;
 
 return static function (ContainerConfigurator $container) {
     $services = $container->services();
@@ -39,8 +41,26 @@ return static function (ContainerConfigurator $container) {
 
     $services
         ->set(WopiDiscovery::class)
-        ->arg('$configuration', '%wopi%');
+        ->arg('$configuration', '%wopi%')
+        ->arg('$client', service('wopi_bundle.http_client'));
 
     $services
         ->alias(WopiDiscoveryInterface::class, WopiDiscovery::class);
+
+    $services
+        ->set('wopi_bundle.cached_http_client')
+        ->class(CachingHttpClient::class)
+        ->decorate('http_client')
+        ->args([
+            service('.inner'),
+            service('http_cache.store'),
+        ]);
+
+    $services
+        ->set('wopi_bundle.http_client')
+        ->class(Psr18Client::class)
+        ->decorate('psr18.http_client')
+        ->args([
+            service('wopi_bundle.cached_http_client')
+        ]);
 };
