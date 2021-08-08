0fd65baeadb53b150c71b15cb25a526a60fa4a6c
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-08-08 20:23:04 +0200

    refactor: Update router.

--- src/Resources/config/routes/routes.php
+++ src/Resources/config/routes/routes.php
@@ -8,6 +8,7 @@
 declare(strict_types=1);
 
 use ChampsLibres\WopiBundle\Controller\Files;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 /**
@@ -16,98 +17,282 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
  * phpcs:disable Generic.Files.LineLength.TooLong
  */
 return static function (RoutingConfigurator $routes) {
+    $fileIdRegex = '^[\w,\s-]+\.[\w,\s-]+$';
+
+    /** Conditions shortcuts */
+    $hasAccessTokenQueryParam = 'request.query.has("access_token")';
+    $hasAccessTokenTTLQueryParam = 'request.query.has("access_token_ttl")';
+    $hasXWOPILockHeader = 'request.headers.has("X-WOPI-Lock")';
+
+    $hasXWopiOverrideHeaderSetTo = static function (string $value): string {
+        return sprintf('request.headers.get("X-WOPI-Override") === "%s"', $value);
+    };
+
+    $hasHeader = static function (string $header): string {
+        return sprintf('request.headers.has("%s")', $header);
+    };
+
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/CheckFileInfo.html
+     */
     $routes
         ->add('checkFileInfo', '/files/{fileId}')
         ->controller([Files::class, 'checkFileInfo'])
-        ->methods(['GET'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$']);
+        ->methods([Request::METHOD_GET])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                ]
+            )
+        );
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/GetFile.html
+     */
     $routes
         ->add('getFile', '/files/{fileId}/contents')
         ->controller([Files::class, 'getFile'])
-        ->methods(['GET'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$']);
+        ->methods([Request::METHOD_GET])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                ]
+            )
+        );
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/Lock.html
+     */
     $routes
         ->add('lock', '/files/{fileId}')
         ->controller([Files::class, 'lock'])
-        ->methods(['POST'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$'])
-        ->condition('request.headers.get("X-WOPI-Override") === "LOCK" and request.headers.get("X-WOPI-Lock") !== null');
+        ->methods([Request::METHOD_POST])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                    $hasXWOPILockHeader,
+                    $hasXWopiOverrideHeaderSetTo('LOCK'),
+                ]
+            )
+        );
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/GetLock.html
+     */
     $routes
         ->add('getLock', '/files/{fileId}')
         ->controller([Files::class, 'getLock'])
-        ->methods(['POST'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$'])
-        ->condition('request.headers.get("X-WOPI-Override") === "GET_LOCK"');
+        ->methods([Request::METHOD_POST])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition('request.headers.get("X-WOPI-Override") === "GET_LOCK"')
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                    $hasXWOPILockHeader,
+                    $hasXWopiOverrideHeaderSetTo('GET_LOCK'),
+                ]
+            )
+        );
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/RefreshLock.html
+     */
     $routes
         ->add('refreshLock', '/files/{fileId}')
         ->controller([Files::class, 'refreshLock'])
-        ->methods(['POST'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$'])
-        ->condition('request.headers.get("X-WOPI-Override") === "REFRESH_LOCK" and request.headers.get("X-WOPI-Lock") !== null');
+        ->methods([Request::METHOD_POST])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                    $hasXWOPILockHeader,
+                    $hasXWopiOverrideHeaderSetTo('REFRESH_LOCK'),
+                ]
+            )
+        );
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/Unlock.html
+     */
     $routes
         ->add('unlock', '/files/{fileId}')
         ->controller([Files::class, 'unlock'])
-        ->methods(['POST'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$'])
-        ->condition('request.headers.get("X-WOPI-Override") === "UNLOCK" and request.headers.get("X-WOPI-Lock") !== null');
+        ->methods([Request::METHOD_POST])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                    $hasXWOPILockHeader,
+                    $hasXWopiOverrideHeaderSetTo('UNLOCK'),
+                ]
+            )
+        );
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/UnlockAndRelock.html
+     */
     $routes
         ->add('unlockAndRelock', '/files/{fileId}')
         ->controller([Files::class, 'unlockAndRelock'])
-        ->methods(['POST'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$'])
-        ->condition('request.headers.get("X-WOPI-Override") === "LOCK" and request.headers.get("X-WOPI-Lock") !== null and request.headers.get("X-WOPI-OldLock") !== null');
+        ->methods([Request::METHOD_POST])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                    $hasXWOPILockHeader,
+                    $hasXWopiOverrideHeaderSetTo('LOCK'),
+                    $hasHeader('X-WOPI-OldLock'),
+                ]
+            )
+        );
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/PutFile.html
+     */
     $routes
         ->add('putFile', '/files/{fileId}/contents')
         ->controller([Files::class, 'putFile'])
-        ->methods(['POST'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$']);
+        ->methods([Request::METHOD_POST])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                    $hasXWopiOverrideHeaderSetTo('PUT'),
+                ]
+            )
+        );
 
-    // TODO: Incomplete
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/PutRelativeFile.html
+     */
     $routes
         ->add('putRelativeFile', '/files/{fileId}')
         ->controller([Files::class, 'putRelativeFile'])
-        ->methods(['POST'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$'])
-        ->condition('request.headers.get("X-WOPI-Override") === "PUT_RELATIVE"');
+        ->methods([Request::METHOD_POST])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                    $hasXWopiOverrideHeaderSetTo('PUT_RELATIVE'),
+                ]
+            )
+        );
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/RenameFile.html
+     */
     $routes
         ->add('renameFile', '/files/{fileId}')
         ->controller([Files::class, 'renameFile'])
-        ->methods(['POST'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$'])
-        ->condition('request.headers.get("X-WOPI-Override") === "RENAME_FILE" and request.headers.get("X-WOPI-Lock") !== null and request.headers.get("X-WOPI-RequestedName") !== null');
+        ->methods([Request::METHOD_POST])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                    $hasXWOPILockHeader,
+                    $hasXWopiOverrideHeaderSetTo('RENAME_FILE'),
+                    $hasHeader('X-WOPI-RequestedName'),
+                ]
+            )
+        );
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/DeleteFile.html
+     */
     $routes
         ->add('deleteFile', '/files/{fileId}')
         ->controller([Files::class, 'deleteFile'])
-        ->methods(['POST'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$'])
-        ->condition('request.headers.get("X-WOPI-Override") === "DELETE"');
+        ->methods([Request::METHOD_POST])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                    $hasXWopiOverrideHeaderSetTo('DELETE'),
+                ]
+            )
+        );
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/EnumerateAncestors.html
+     */
     $routes
         ->add('enumerateAncestors', '/files/{fileId}/ancestry')
         ->controller([Files::class, 'enumerateAncestors'])
-        ->methods(['GET'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$']);
+        ->methods([Request::METHOD_GET])
+        ->requirements(['fileId' => $fileIdRegex]);
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/GetShareUrl.html
+     */
     $routes
         ->add('getShareUrl', '/files/{fileId}')
         ->controller([Files::class, 'getShareUrl'])
-        ->methods(['POST'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$'])
-        ->condition('request.headers.get("X-WOPI-Override") === "GET_SHARE_URL" and request.headers.get("X-WOPI-UrlType") !== null');
+        ->methods([Request::METHOD_POST])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                    $hasXWopiOverrideHeaderSetTo('GET_SHARE_URL'),
+                    $hasHeader('X-WOPI-UrlType'),
+                ]
+            )
+        );
 
+    /**
+     * @see https://wopi.readthedocs.io/projects/wopirest/en/latest/files/PutUserInfo.html
+     */
     $routes
         ->add('putUserInfo', '/files/{fileId}')
         ->controller([Files::class, 'putUserInfo'])
-        ->methods(['POST'])
-        ->requirements(['fileId' => '^[\w,\s-]+\.[\w,\s-]+$'])
-        ->condition('request.headers.get("X-WOPI-Override") === "PUT_USER_INFO"');
+        ->methods([Request::METHOD_POST])
+        ->requirements(['fileId' => $fileIdRegex])
+        ->condition(
+            implode(
+                ' and ',
+                [
+                    $hasAccessTokenQueryParam,
+                    $hasAccessTokenTTLQueryParam,
+                    $hasXWopiOverrideHeaderSetTo('PUT_USER_INFO'),
+                ]
+            )
+        );
 };
