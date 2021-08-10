272696104b2dfd7191a93423519e15cd4429a76f
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-08-10 08:09:45 +0200

    Autofix code style.

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use ChampsLibres\WopiBundle\EventListener\WopiDiscoveryListener;
 use ChampsLibres\WopiLib\Discovery\WopiDiscovery;
 use ChampsLibres\WopiLib\Discovery\WopiDiscoveryInterface;
 use Symfony\Component\HttpClient\CachingHttpClient;
